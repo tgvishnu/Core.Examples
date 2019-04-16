9a04a206d2c6cd8557c90cd0f9294ec12ee4be5f
vishnu <tg.vishnu@gmail.com>
2019-04-16 10:23:15 +0530

    Middleware - Sample added

---
+++ ML.SampleExample/EnvironmentMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ML.SampleExample
+{
+    internal class EnvironmentMiddleware
+    {
+        public EnvironmentMiddleware(RequestDelegate next, IHostingEnvironment environment)
+        {
+            this.Next = next;
+            this.EnvironementName = environment.EnvironmentName;
+        }
+
+        public RequestDelegate Next { get; private set; }
+        public string EnvironementName { get; private set; }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var myTimer = Stopwatch.StartNew();
+            context.Response.Headers.Add("X-HostingEnvironmentName", new[] { EnvironementName });
+            await Next(context);
+            if(context.Request.ContentType != null && context.Request.ContentType.Contains("html"))
+            {
+                await context.Response.WriteAsync($"<p> from {EnvironementName} in {myTimer.ElapsedMilliseconds} ms </p>");
+            }
+        }
+    }
+}
---
+++ ML.SampleExample/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ML.SampleExample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Information);
+            var logger = loggerFactory.CreateLogger("Middleware Demo");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<EnvironmentMiddleware>();
+
+            app.UseStaticFiles();
+            app.Use(async (context, next) =>
+            {
+                var myTimer = System.Diagnostics.Stopwatch.StartNew();
+                logger.LogInformation($"--===> Beginning request is {env.EnvironmentName} environment");
+                await next();
+                logger.LogInformation($"--===> Request completed in {myTimer.ElapsedMilliseconds} ms");
+            });
+            app.Map("/stuff", appl => appl.Run(async (context) =>
+           {
+               context.Request.ContentType = "text/html";
+               await context.Response.WriteAsync("This is your stuff");
+           }));
+
+            app.MapWhen(context => context.Request.Headers["User-Agent"].Contains("Apple-iphone"), iphoneRoute);
+
+            app.Run(async (context) =>
+            {
+                context.Request.ContentType = "text/html";
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void iphoneRoute(IApplicationBuilder obj)
+        {
+            obj.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Iphone agent");
+            });
+        }
+    }
+}
